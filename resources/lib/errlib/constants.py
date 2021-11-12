33da34690fc199b346310faa3247f20cb554c0b6
Üllar Pajus <yllar.pajus@gmail.com>
2021-11-12 19:23:01 +0200

    reinstate kodi 18 support, thanks Priit

--- resources/lib/errlib/constants.py
+++ resources/lib/errlib/constants.py
@@ -1,4 +1,4 @@
-
+# coding: utf-8
 ERR_API_VERSION = 'v2'
 ERR_API_BASEURL = 'https://services.err.ee/api/'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'
--- resources/lib/errlib/search.py
+++ resources/lib/errlib/search.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import json
 
 from .helpers import download_url
